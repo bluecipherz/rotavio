aa70de537de937f486b09651f36d71ad0fa7d48e
Rinas Musthafa <rinasmusthafa@.live.com>
2017-02-03 01:53:28 +0530

    heli image updated, ui-sref-updated,

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,54 +9,66 @@
  * Main module of the application.
  */
 angular
-  .module('BczUiApp', [
-    'ngResource',
-    'ui.router',
-    'ngCookies',
-    'satellizer',
-    'ngAnimate',
-    'rzModule',
-    'ngSanitize',
-    'angular-svg-round-progressbar',
-    // 'datePicker'
-  ])
-  .config(function ($locationProvider,$stateProvider,$urlRouterProvider, $authProvider) {
+    .module('BczUiApp', [
+        'ngResource',
+        'ui.router',
+        'ngCookies',
+        'satellizer',
+        'ngAnimate',
+        'rzModule',
+        'ngSanitize',
+        'angular-svg-round-progressbar',
+        // 'datePicker'
+    ])
+    .config(function ($locationProvider,$stateProvider,$urlRouterProvider, $authProvider) {
 
-      // Satellizer configuration that specifies which API
-      // route the JWT should be retrieved from
-      // $authProvider.loginUrl = 'http://api.blue.bluroe.com/api/authenticate';
-      $authProvider.baseUrl = 'http://localhost:9000';
-      $authProvider.loginUrl = 'http://api.blue.bluroe.com/api/authenticate';
+        // Satellizer configuration that specifies which API
+        // route the JWT should be retrieved from
+        // $authProvider.loginUrl = 'http://api.blue.bluroe.com/api/authenticate';
+        $authProvider.baseUrl = 'http://localhost:9000';
+        $authProvider.loginUrl = 'http://api.blue.bluroe.com/api/authenticate';
 
-      $urlRouterProvider.otherwise('/');
+        $urlRouterProvider.otherwise('/');
 
-      $stateProvider
-        .state('home', {
-          url: '/',
-          templateUrl: 'views/home.html',
-          controller: 'MainCtrl',
-          controllerAs: 'vm'
-        })
-        .state('whoweare', {
-          url: '/whoweare',
-          templateUrl: 'views/whoweare.html',
-          controller: 'WhoweareCtrl',
-          controllerAs: 'vm'
-        })
-        .state('demo', {
-          url: '/demo',
-          templateUrl: 'views/demo.html',
-          controller: 'DemoCtrl',
-          controllerAs: 'vm'
-        })
-        .state('explore', {
-          url: '/explore',
-          templateUrl: 'views/explore.html',
-          controller: 'ExploreCtrl',
-          controllerAs: 'vm'
-        })
-  })
-.run(function($rootScope, $location, $state, $stateParams, $cookieStore) {
+        $stateProvider
+            .state('home', {
+                url: '/',
+                templateUrl: 'views/home.html',
+                controller: 'MainCtrl',
+                controllerAs: 'vm'
+            })
+            .state('whoweare', {
+                url: '/whoweare',
+                templateUrl: 'views/whoweare.html',
+                controller: 'WhoweareCtrl',
+                controllerAs: 'vm'
+            })
+            .state('demo', {
+                url: '/demo',
+                templateUrl: 'views/demo.html',
+                controller: 'DemoCtrl',
+                controllerAs: 'vm'
+            })
+            .state('explore', {
+                url: '/explore',
+                templateUrl: 'views/explore.html',
+                controller: 'ExploreCtrl',
+                controllerAs: 'vm'
+            })
+            .state('exploreServo', {
+                url: '/explore/servo',
+                templateUrl: 'views/explore.html',
+                controller: 'ExploreCtrl',
+                controllerAs: 'vm'
+            })
+            .state('exploreSoftware', {
+                url: '/explore/software',
+                templateUrl: 'views/explore.html',
+                controller: 'ExploreCtrl',
+                controllerAs: 'vm'
+            })
+    })
+    .run(function($rootScope, $location, $state, $stateParams, $cookieStore) {
 
-});
+    });
 
